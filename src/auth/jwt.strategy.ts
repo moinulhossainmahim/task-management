700a0275021435c288775f3f7b98da6e2f3ed881
Moinul Hossain <moinulhossainmahim@gmail.com>
2022-11-23 18:32:52 +0600

    add configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
@@ -10,12 +11,13 @@ import { User } from "./user.entity";
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>
+    private usersRepository: Repository<User>,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    })
+    });
   }
 
   async validate(payload: JwtPayload): Promise<User> {
